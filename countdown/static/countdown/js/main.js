21d781dee9759cb7b329a8a5e198df987a543355
kaitaylor <kai.taylor.me@gmail.com>
2015-06-12 17:16:28 +0100

    WIP create new countdown form

--- countdown/static/countdown/js/main.js
+++ countdown/static/countdown/js/main.js
@@ -21,6 +21,44 @@ app.initialize = function () {
         $('.modal__new-countdown').removeClass('show');
     });
 
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+
+    $('.new-countdown-date').pickmeup({
+        flat    : false,
+        min     : dd+'-'+mm+'-'+yyyy
+    });
+
+    $('.submit input').on('click', function() {
+
+        console.log('KAI :: submit click handler');
+        app.createNewCountdown();
+    });
+};
+
+
+app.createNewCountdown = function() {
+    console.log('KAI :: creating new countdown');
+
+    var title = "test";
+    var description = "test description";
+    var end_datetime = new Date();
+
+    $.ajax({
+        type: "POST",
+        data: "title=" + title + "&description=" + description + "&end_datetime=" + end_datetime,
+        url: "/api/countdowns/create" ,
+        success: function(data) {
+            if (data) {
+                console.log('KAI ::', data);
+            }
+        },
+        error: function(){
+            console.log('KAI :: an error occured');
+        }
+    });
 };
 
 $( document ).ready(function() {
